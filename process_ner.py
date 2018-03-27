69b968c2e2041f0141082f8af906a368d8fc67e6
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2018-03-27 06:26:23 -0400

    Filter out header info

--- process_ner.py
+++ process_ner.py
@@ -11,3 +11,16 @@ def get_articles_from_file(path):
     with open(path, 'r') as f:
         articles = [json.loads(article) for article in f.readlines()]
     return articles
+
+
+def filter_out_invalid(articles):
+    """ For < 2 paras only agency info and date are present."""
+    return [a for a in articles if len(a['text']) > 2]
+
+
+def filter_out_header(para_list):
+    
+    def not_x_min_read(para):
+        return False if para.endswith('Min Read') and len(para.split()) == 3 else True
+    
+    return [p for p in para_list if not_x_min_read(p) and p != 'Reuters Staff']
