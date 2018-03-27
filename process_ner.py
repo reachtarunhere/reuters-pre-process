c18b3fc4d6450dcb56ea8e3ae7c2c3eb74437381
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2018-03-27 05:46:38 -0400

    Read articles from single file

--- process_ner.py
+++ process_ner.py
@@ -1,6 +1,13 @@
 import glob
+import json
 
 # Helper Functions
 
 
 def get_all_files(root): return glob.glob(root + "/*/*/*.json")
+
+
+def get_articles_from_file(path):
+    with open(path, 'r') as f:
+        articles = [json.loads(article) for article in f.readlines()]
+    return articles
