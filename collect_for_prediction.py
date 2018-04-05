951211678d0c3f85593ef1b1cf04b99a05ca29fd
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2018-04-06 04:19:21 +0800

    for multi document labeling

--- collect_for_prediction.py
+++ collect_for_prediction.py
@@ -6,7 +6,7 @@ def get_all_files(root): return glob.glob(root + "/*/*/*.copr")
 def process_single_file(path, output_file):
     with open(path, 'r') as f:
         lines = f.readlines()
-    lines = lines[1:]
+    lines = lines + ["\n"]
     output_file.writelines(lines)
 
 if __name__ == '__main__':
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     all_files = get_all_files(crawled_folder)
 
     output_file = open(output_file_path, 'a', encoding='utf-8')
-    output_file.writelines(['-DOCSTART-\n'])
 
     for load_path in all_files:
 
