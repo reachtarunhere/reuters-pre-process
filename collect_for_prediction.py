b0a02cd2e0030602e934f5650de42ee97aa2b7e1
Tarun Kumar Vangani <reachtarunhere@gmail.com>
2018-04-05 10:54:07 -0400

    reduce to single file so model does not need multiple loading

---
+++ collect_for_prediction.py
@@ -0,0 +1,24 @@
+import glob
+
+def get_all_files(root): return glob.glob(root + "/*/*/*.copr")
+
+def process_single_file(path, output_file):
+    with open(path, 'r') as f:
+        lines = f.readlines()
+    lines = lines[2:]
+    output_file.writelines(lines)
+
+if __name__ == '__main__':
+
+    crawled_folder = sys.argv[1]  # Lazy hack. Could use argparse instead.
+    output_file_path = sys.argv[2]
+    all_files = get_all_files(crawled_folder)
+
+    output_file = open(output_file_path, 'a', encoding='utf-8')
+    output_file.writelines(['-DOCSTART-\n'])
+
+    for load_path in all_files:
+
+        print("Processing", load_path)
+        process_single_file(load_path, output_file)
+
